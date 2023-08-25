1f688052fa791f719ac57ea125d74e9b2b1f422d
Diego Cáceres-Cardoza <diegokaiser@gmail.com>
2023-08-24 19:16:06 -0500

    add mongodb

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,4 +1,4 @@
-import { MovieModel } from "../models/movie"
+import { MovieModel } from "../models/local-file-system/movie.js"
 import { validateMovie, validatePartialMovie } from './schemas/movies.js'
 
 export class MovieController {
