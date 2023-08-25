7d9d237d5299bc0c79785209acd0d56f5c361682
Diego Cáceres-Cardoza <diegokaiser@gmail.com>
2023-08-24 19:30:08 -0500

    fix ../../utils.js

--- models/local-file-system/movie.js
+++ models/local-file-system/movie.js
@@ -1,5 +1,5 @@
 import { randomUUID } from 'node:crypto'
-import { readJSON } from '../utils'
+import { readJSON } from '../../utils.js'
 
 // import del futuro, aun no
 // import movies fron './movies.sjon with { type: 'json' }
